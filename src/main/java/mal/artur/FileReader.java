fce5ec044487726ab30b0ea3fe7f671ccb447f06
arturmalinowski <artur@wow.com>
2018-05-29 20:01:55 +0100

    File reader added

---
+++ src/main/java/mal/artur/FileReader.java
@@ -0,0 +1,32 @@
+package mal.artur;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReader {
+
+    private List<String> lines;
+
+    public FileReader(){}
+
+    public List<String> readFrom(File file){
+        lines = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new java.io.FileReader(file))) {
+            String line = br.readLine();
+
+            while (line != null) {
+                lines.add(line);
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+}
---
+++ src/test/java/mal/artur/FileReaderTest.java
@@ -0,0 +1,27 @@
+package mal.artur;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class FileReaderTest {
+
+    private FileReader underTest;
+
+    @Before
+    public void setUp(){
+        underTest = new FileReader();
+    }
+
+    @Test
+    public void canReadFile() {
+        File file = new File("src/test/resources/AddressBook");
+        List<String> addressBook = underTest.readFrom(file);
+        assertThat(addressBook.size(), is(5));
+    }
+}
